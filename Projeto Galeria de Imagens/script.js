2bd2ef7c249fa737a43056e7f0239a66e1f3347c
Yuri <yuri.silva.cardoso03@aluno.ifce.edu.br>
2023-11-16 08:50:31 -0300

    Projeto Galeria de Imagens

---
+++ Projeto Galeria de Imagens/script.js
@@ -0,0 +1,12 @@
+function openLightbox(imageSrc) {
+    var lightbox = document.getElementById('lightbox');
+    var lightboxImg = document.getElementById('lightbox-img');
+
+    lightboxImg.src = imageSrc;
+    lightbox.style.display = 'flex';
+}
+
+function closeLightbox() {
+    var lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
+}
